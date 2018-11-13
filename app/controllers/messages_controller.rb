f438a6b580440ff5eaf15e7d91221926af037184
Tobias Aires <tobiasaires@gmail.com>
2018-11-12 22:39:51 -0300

    Criado metodos de CRUD

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,50 @@
+class MessagesController < ApplicationController
+    before_action :find_message, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @messages = Message.all.order("created_at DESC")
+    end
+
+    def new
+        @message = Message.new
+    end
+
+    def show
+    end
+    
+    def edit
+    end
+    
+    def update
+        if @message.update(message_params)
+            redirect_to message_path
+        else
+            redirect_to 'edit'
+        end
+    end
+
+    def destroy
+        if @message.destroy
+            redirect_to root_path
+        end
+    end
+
+    def create
+        @message = Message.new(message_params)
+        if @message.save
+            redirect_to root_path
+        else
+            redirect_to 'new'
+        end
+    end
+
+    private
+        def message_params
+            params.require(:message).permit(:title, :description)
+        end
+        
+        def find_message
+            @message = Message.find(params[:id])
+        end
+            
+end
