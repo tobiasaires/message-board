62c0ddfef6c8bc5a49977039fd44ca5f796c6b7b
Tobias Aires <tobiasaires@gmail.com>
2018-11-13 21:39:46 -0300

    adicionado logica de usuario

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,7 @@ class MessagesController < ApplicationController
     end
 
     def new
-        @message = Message.new
+        @message = current_user.messages.build
     end
 
     def show
@@ -30,7 +30,7 @@ class MessagesController < ApplicationController
     end
 
     def create
-        @message = Message.new(message_params)
+        @message = current_user.messages.build(message_params)
         if @message.save
             redirect_to root_path
         else
