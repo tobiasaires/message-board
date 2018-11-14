49cd0386340120f40e276262c47559aecd778d4b
Tobias Aires <tobiasaires@gmail.com>
2018-11-13 22:36:28 -0300

    adicionado feature de comentario

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   
   has_many :messages
+  has_many :comments
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
